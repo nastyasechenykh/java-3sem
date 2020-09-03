2e3cac2e344a2cedc9cb85336028995f670db63f
Nastya <nastya.sechenykh@mail.ru>
2020-09-03 17:46:03 +0300

    First Lesson

--- src/Poem.java
+++ src/Poem.java
@@ -1,2 +1,16 @@
 public class Poem {
+    private String[] poem;
+
+    public Poem(String[] poem){
+        this.poem = poem;
+    }
+
+    public void findPhymes(){
+        for (int k = 0; k < poem.length - 1; k++) {
+            for (int i = 1; i < poem.length; i++) {
+                if (poem[k].substring(poem[k].length() - 3, poem[k].length() - 1).equals(poem[i].substring(poem[i].length() - 3, poem[i].length() - 1)) && (i != k))
+                    System.out.println("Строка " + (k + 1) + " рифмуется со строкой " + (i + 1));
+            }
+        }
+    }
 }
--- src/PoemCheck.java
+++ src/PoemCheck.java
@@ -1,2 +1,7 @@
 public class PoemCheck {
+    public static void main(String[] args) {
+        String[] s = {"Кошка", "Пень", "Картошка", "День"};
+        Poem p = new Poem(s);
+        p.findPhymes();
+    }
 }
