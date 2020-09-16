61a6e2397d5749b2a423381b00ee8c15aeeaff56
Nastya <nastya.sechenykh@mail.ru>
2020-09-16 23:47:50 +0300

    some more tasks

--- src/Collections.java
+++ src/Collections.java
@@ -6,6 +6,7 @@ public class Collections {
         ArrayList<String> l1 = new ArrayList<>();
         ArrayList<String> l2 = new ArrayList<>();
         ArrayList<String> l3 = new ArrayList<>();
+        ArrayList<String> l4 = new ArrayList<>();
         l1.add("a");
         l1.add("b");
         l1.add("c");
@@ -14,12 +15,25 @@ public class Collections {
         l3.add("be");
         l3.add("le");
         l3.add("kA");
+        l4.add("p");
+        l4.add("q");
+        l4.add("e");
+        l4.add("k");
 
+        System.out.println("n. 1");
         System.out.println(makeList());
+        System.out.println("n. 2");
         joinLists(l1, l2);
-        clearReverseList(l3);
-        reverseList(l3);
-        //clearDeleteEven(l3);
+        System.out.println("n. 4");
+        clearReverseList(l3); //функции clearReverseList и
+        reverseList(l3); //reverseList реализованы без использования метода size
+        System.out.println("n. 5");
+        clearDeleteEven(l1);
+        deleteEven(l4);
+
+
+        clearDeleteInInteger(makeList());
+        deleteInInteger(makeList());
     }
 
     public static List<Integer> makeList() {
@@ -46,7 +60,7 @@ public class Collections {
         System.out.println(a); //старый остался неизмененным
     }
 
-    public static void reverseList(List<String> a){
+    public static void reverseList(List<String> a) {
         String c = "";
         int k = 0;
 
@@ -55,10 +69,55 @@ public class Collections {
         System.out.println(a);
     }
 
-    //public static void clearDeleteEven(List<String> a) {
-    //    ArrayList<String> copyA = new ArrayList<>();
+    public static void clearDeleteEven(List<String> a) {
+        ArrayList<String> copyA = new ArrayList<>();
 
-    //    for (String x : a) copyA.add(x);
+        copyA.addAll(a);
+        for (int i = 0; i < copyA.size(); i++) {
+            if (i % 2 == 0)
+                copyA.remove(i);
+        }
+        System.out.println(a); //неизменен
+        System.out.println(copyA); //новый список
+    }
 
-    //}
+    public static void deleteEven(ArrayList<String> a) {
+
+        for (int i = 0; i < a.size(); i++) {
+            if (i % 2 == 0)
+                a.remove(i);
+        }
+        System.out.println(a); //подмена старого списка
+    }
+
+    /* public static void clearDeleteInString(List<String> a) {
+
+    }
+
+    public static void deleteInString(List<String> a) {
+
+        for (String x: a){
+            if (x % 2 == 0) a.remove(x);
+        }
+        System.out.println(a);
+    } */
+
+    public static void clearDeleteInInteger(List<Integer> a) {
+        ArrayList<Integer> copyAa = new ArrayList<>();
+
+        copyAa.addAll(a);
+        for (int i = 0; i < copyAa.size(); i++) {
+            if (copyAa.get(i) % 2 == 0) copyAa.remove(i);
+        }
+        System.out.println(a); //исходный список
+        System.out.println(copyAa); //скопированный список
+    }
+
+    public static void deleteInInteger(List<Integer> a) {
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) % 2 == 0) a.remove(i);
+        }
+        System.out.println(a);
+    }
 }
